efe45956af7c7df8644113cea5c79029c11b096c
Lizari <muroya725@gmail.com>
2020-11-29 04:05:34 +0900

    [Fix] permission not being updated

--- src/main/java/network/atria/RankSystem/CheckRankCommand.java
+++ src/main/java/network/atria/RankSystem/CheckRankCommand.java
@@ -3,7 +3,6 @@ package network.atria.RankSystem;
 import app.ashcon.intake.Command;
 import app.ashcon.intake.bukkit.parametric.annotation.Sender;
 import java.util.UUID;
-import network.atria.Database.MySQLSetterGetter;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 
@@ -15,18 +14,21 @@ public class CheckRankCommand {
   public void rank(@Sender Player sender) {
 
     final UUID uuid = sender.getUniqueId();
-    final String current = MySQLSetterGetter.getRank(uuid);
+    final String current = Ranks.getRankCurrent(uuid);
     final String next = Ranks.getNextRank(uuid);
+    final int point = Ranks.getCurrentPoint(uuid);
 
-    sender.sendMessage(
-        ChatColor.DARK_AQUA + "Rank: " + ChatColor.AQUA + current.replace("_", " ").toUpperCase());
-    sender.sendMessage(
+    final String line_1 = ChatColor.DARK_AQUA + "Rank: " + ChatColor.AQUA + current;
+    final String line_2 =
         ChatColor.DARK_AQUA
             + "Your current points are "
             + ChatColor.AQUA
             + ""
             + ChatColor.BOLD
-            + Ranks.getCurrentPoint(uuid));
+            + point;
+
+    sender.sendMessage(line_1 + "\n" + line_2);
+
     if (next.equalsIgnoreCase(current)) {
       sender.sendMessage(
           ChatColor.DARK_AQUA + "You can't ranked up because you're " + current.toUpperCase());
--- src/main/java/network/atria/RankSystem/Ranks.java
+++ src/main/java/network/atria/RankSystem/Ranks.java
@@ -2,7 +2,6 @@ package network.atria.RankSystem;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 import network.atria.Database.MySQLSetterGetter;
 import network.atria.Util.RanksConfig;
@@ -66,35 +65,35 @@ public class Ranks {
       case "wood_iii":
       case "wood_ii":
       case "wood_i":
-        rank = ChatColor.GOLD + "" + ChatColor.BOLD + current;
+        rank = ChatColor.GOLD + "" + ChatColor.BOLD + format(current);
         break;
       case "stone_iii":
       case "stone_ii":
       case "stone_i":
-        rank = ChatColor.GRAY + "" + ChatColor.BOLD + current;
+        rank = ChatColor.GRAY + "" + ChatColor.BOLD + format(current);
         break;
       case "iron_iii":
       case "iron_ii":
       case "iron_i":
-        rank = ChatColor.WHITE + "" + ChatColor.BOLD + current;
+        rank = ChatColor.WHITE + "" + ChatColor.BOLD + format(current);
         break;
       case "gold_iii":
       case "gold_ii":
       case "gold_i":
-        rank = ChatColor.YELLOW + "" + ChatColor.BOLD + current;
+        rank = ChatColor.YELLOW + "" + ChatColor.BOLD + format(current);
         break;
       case "emerald_iii":
       case "emerald_ii":
       case "emerald_i":
-        rank = ChatColor.GREEN + "" + ChatColor.BOLD + current;
+        rank = ChatColor.GREEN + "" + ChatColor.BOLD + format(current);
         break;
       case "diamond_iii":
       case "diamond_ii":
       case "diamond_i":
-        rank = ChatColor.AQUA + "" + ChatColor.BOLD + current;
+        rank = ChatColor.AQUA + "" + ChatColor.BOLD + format(current);
         break;
       case "obsidian":
-        rank = ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + current;
+        rank = ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + format(current);
     }
 
     return rank;
@@ -107,44 +106,46 @@ public class Ranks {
 
   public static String getRankNext(UUID uuid) {
     String rank = null;
-    switch (Objects.requireNonNull(getNextRank(uuid))) {
+    final String next = getNextRank(uuid);
+
+    switch (next) {
       case "wood_iii":
       case "wood_ii":
       case "wood_i":
-        rank = ChatColor.GOLD + "" + ChatColor.BOLD + format(uuid);
+        rank = ChatColor.GOLD + "" + ChatColor.BOLD + format(next);
         break;
       case "stone_iii":
       case "stone_ii":
       case "stone_i":
-        rank = ChatColor.GRAY + "" + ChatColor.BOLD + format(uuid);
+        rank = ChatColor.GRAY + "" + ChatColor.BOLD + format(next);
         break;
       case "iron_iii":
       case "iron_ii":
       case "iron_i":
-        rank = ChatColor.WHITE + "" + ChatColor.BOLD + format(uuid);
+        rank = ChatColor.WHITE + "" + ChatColor.BOLD + format(next);
         break;
       case "gold_iii":
       case "gold_ii":
       case "gold_i":
-        rank = ChatColor.YELLOW + "" + ChatColor.BOLD + format(uuid);
+        rank = ChatColor.YELLOW + "" + ChatColor.BOLD + format(next);
         break;
       case "emerald_iii":
       case "emerald_ii":
       case "emerald_i":
-        rank = ChatColor.GREEN + "" + ChatColor.BOLD + format(uuid);
+        rank = ChatColor.GREEN + "" + ChatColor.BOLD + format(next);
         break;
       case "diamond_iii":
       case "diamond_ii":
       case "diamond_i":
-        rank = ChatColor.AQUA + "" + ChatColor.BOLD + format(uuid);
+        rank = ChatColor.AQUA + "" + ChatColor.BOLD + format(next);
         break;
       case "obsidian":
-        rank = ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + format(uuid);
+        rank = ChatColor.DARK_PURPLE + "" + ChatColor.BOLD + format(next);
     }
     return rank;
   }
 
-  private static String format(UUID uuid) {
-    return Objects.requireNonNull(getNextRank(uuid)).replace("_", " ").toUpperCase();
+  private static String format(String rank) {
+    return rank.replace("_", " ").toUpperCase();
   }
 }
